1a88684c4fd3dbd51189ab532378c829429432c6
sarakhandaker <kttsara@gmail.com>
2020-10-16 14:26:54 -0700

    done

--- recurring_moment.rb
+++ recurring_moment.rb
@@ -1,5 +1,6 @@
 require 'active_support'
 require 'active_support/core_ext'
+require 'pry'
 
 class RecurringMoment
   def initialize(start:, interval:, period:)
@@ -10,21 +11,18 @@ class RecurringMoment
 
   def match(date)
     current = @start
-
+    i=1
     while current < date
-      if current == date
-        return true
-      end
-
       if @period == 'monthly'
+       current = @start.advance(months: @interval*i)
+        i+=1
+      elsif @period == 'weekly'
         current = current.advance(weeks: @interval)
-      elsif @period = 'weekly'
-        current = current.advance(months: @interval)
       elsif @period == 'daily'
         current = current.advance(days: @interval)
       end
     end
 
-    return false
+    return current == date
   end
 end
